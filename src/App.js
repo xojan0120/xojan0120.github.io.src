813e24bcf5d7862656c9d366cc8a17aa284624fb
xojan0120 <xojan0120@gmail.com>
2019-05-17 21:23:47 +0900

    [Update]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import FileCopy from '@material-ui/icons/FileCopy';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function App() {
+//class App extends React.Component {
+//  state = {
+//    value: '',
+//    copied: false,
+//  };
+//
+//  render() {
+//    return (
+//      <div>
+//        <input value={this.state.value}
+//          onChange={({target: {value}}) => this.setState({value, copied: false})} />
+//
+//        <CopyToClipboard text={this.state.value}
+//          onCopy={() => this.setState({copied: true})}>
+//          <span>Copy to clipboard with span</span>
+//        </CopyToClipboard>
+//
+//        <CopyToClipboard text={this.state.value}
+//          onCopy={() => this.setState({copied: true})}>
+//          <button>Copy to clipboard with button</button>
+//        </CopyToClipboard>
+//
+//        {this.state.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
+//      </div>
+//    );
+//  }
+//}
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    width: '80vw',
+  },
+});
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Code {...props}>
+        puts "hello world"
+      </Code>
     </div>
   );
 }
 
-export default App;
+const Code = (props) => {
+  const { classes } = props;
+  return (
+    <Paper className={classes.root} elevation={1}>
+
+      <Typography variant="h5" component="h3">
+        This is a sheet of paper.
+        <CopyToClipboard text={props.children}>
+          <IconButton className={classes.button}>
+            <FileCopy />
+          </IconButton>
+        </CopyToClipboard>
+      </Typography>
+
+      <Typography component="p">
+        <code>
+          {props.children}
+        </code>
+      </Typography>
+
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(App);
